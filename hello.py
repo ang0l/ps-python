5d70cd2a2fd5b67f52317be677a88349bb9721fb
Головушкин Андрей Владимирович <ang0l@inbox.ru>
2025-10-27 13:24:52 +0300

    Типы

--- hello.py
+++ hello.py
@@ -1,14 +1,17 @@
-# Правильные названия переменных
-num = 5
-my_num = 1
-_my_num = 2
-myNum = 3
-MYNUM = 4
-num1 = 6
-print('Привет')
-print (MYNUM)
+user_age = 18
+is_admin = True
+username = 'Антон'
+user_salary = 100.5
 
-# Неравильные названия переменных
-# 2num = 2
-# my-num = 4
-# my num = 3
+# a = 1
+# b = 4
+
+user_age = '18'
+# допустимая запись, например, координат
+position_x, position_y = 1, 4
+
+type_of_user_age = type(user_age)
+print(type_of_user_age)
+print(type(is_admin))
+print(type(user_salary))
+print(type(username))
