8b1675aa61cf4c5ac35cab25bb90eeff0ea38b8b
Головушкин Андрей Владимирович <ang0l@inbox.ru>
2025-10-27 13:43:57 +0300

    Преобразование типов

--- hello.py
+++ hello.py
@@ -1,17 +1,38 @@
-user_age = 18
-is_admin = True
-username = 'Антон'
-user_salary = 100.5
+x = int(1)
+y = int(1.5) # дробная часть будет отброшена
+z = int("2")
 
-# a = 1
-# b = 4
+x = float(1)
+y = float(1.5) # дробная часть будет отброшена
+z = float("2")
+a = float("2.3")
 
-user_age = '18'
-# допустимая запись, например, координат
-position_x, position_y = 1, 4
+x = str(1)
+y = str(1.5) # дробная часть будет отброшена
+z = str("2")
 
-type_of_user_age = type(user_age)
-print(type_of_user_age)
-print(type(is_admin))
-print(type(user_salary))
-print(type(username))
+z = bool(3) # любая строка кроме пустой = true, любое число кроме 0 = true. остальное false
+
+user_data = "Андрей " + str(56)
+print(user_data)
+
+# user_age = 18
+# is_admin = True
+# username = 'Антон'
+# user_salary = 100.5 + 50
+
+# a = 10 + 'fff'
+
+
+# # a = 1
+# # b = 4
+
+# user_age = '18'
+# # допустимая запись, например, координат
+# position_x, position_y = 1, 4
+
+# type_of_user_age = type(user_age)
+# print(type_of_user_age)
+# print(type(is_admin))
+# print(user_salary)
+# print(type(username))
