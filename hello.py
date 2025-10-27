7aabfb560cdf52cb47ea846229e1c0d9c4a6d329
Головушкин Андрей Владимирович <ang0l@inbox.ru>
2025-10-27 12:55:09 +0300

    Переменные и значения

--- hello.py
+++ hello.py
@@ -1,1 +1,14 @@
+# Правильные названия переменных
+num = 5
+my_num = 1
+_my_num = 2
+myNum = 3
+MYNUM = 4
+num1 = 6
 print('Привет')
+print (MYNUM)
+
+# Неравильные названия переменных
+# 2num = 2
+# my-num = 4
+# my num = 3
