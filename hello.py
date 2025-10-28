b041528c36c333e737b00ebb782a66e952718ad6
Головушкин Андрей Владимирович <ang0l@inbox.ru>
2025-10-28 13:52:20 +0300

    Порядок операторов

--- hello.py
+++ hello.py
@@ -1,6 +1,14 @@
 x = -1
 
-print(x > 0 and x < 10)
-print(x < 0 or x >= 10)
-print(not(x > 0 and x < 10))
-print(not(x > 0) and x < 10)
+print(not(x > 0) and x < 10 - 3 * 4)
+
+# not(x > 0) and x < 10 - 12
+# not(x > 0) and x < -2
+# not(-1 > 0) and -1 < -2
+# not(False) and False
+# True and False
+# False
+print(not(x > 0) and x < (10 - 3) * 4)
+print(True and True or False)
+print(True and (True or False))
+
