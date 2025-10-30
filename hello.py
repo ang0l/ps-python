19082c6cbfee6366e779527e31f3418d55182e79
Головушкин Андрей Владимирович <ang0l@inbox.ru>
2025-10-30 11:57:03 +0300

    Упражнение - Максимальное значение

--- hello.py
+++ hello.py
@@ -1,15 +1,19 @@
-age = input("Введите возраст: ")
+# Максимум среди 3-х чисел с плавающей точкой
+number_one   = float(input("Введите первое число: "))
+number_two   = float(input("Введите второе число: "))
+number_three = float(input("Введите третье число: "))
 
-# is_legal_age = False
+# number_max = 0.0
 
-# if int(age) < 18:
-#     is_legal_age = False
-# else:
-#     is_legal_age = True
+# number_max = number_one if number_one > number_max else number_max
+# number_max = number_two if number_two > number_max else number_max
+# number_max = number_three if number_three > number_max else number_max
 
-# if int(age) > 18:
-#     is_legal_age = True
+# print(number_max)
 
-is_legal_age = True if int(age) >= 18 else False
-
-print(is_legal_age)
+if number_one >= number_two and number_one >= number_three:
+    print(number_one)
+elif number_two >= number_one and number_two >= number_three:
+    print(number_two)
+else:
+    print(number_three)
