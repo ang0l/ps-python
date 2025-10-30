ac2caf6a7fadc98ee928bbc0c266485c230ecaa4
Головушкин Андрей Владимирович <ang0l@inbox.ru>
2025-10-30 13:50:41 +0300

    match case

--- hello.py
+++ hello.py
@@ -1,19 +1,28 @@
 # Максимум среди 3-х чисел с плавающей точкой
-number_one   = float(input("Введите первое число: "))
-number_two   = float(input("Введите второе число: "))
-number_three = float(input("Введите третье число: "))
+role = input("Введите роль: ")
+a = 10
 
-# number_max = 0.0
+# if role == 'admin':
+#     print("Админ")
+# elif role == 'manager':
+#     print("Менеджер")
+# elif role == 'seo':
+#     print("SEO специалист")
+# else:
+#     print("Уборщица")
 
-# number_max = number_one if number_one > number_max else number_max
-# number_max = number_two if number_two > number_max else number_max
-# number_max = number_three if number_three > number_max else number_max
+match role:
+    case "admin" | "ADMIN" if a < 0:
+        print("Админ")
+    case "manager":
+        print("Менеджер")
+    case "seo":
+        print("SEO специалист")
+    case _:
+        print("Уборщица")
 
-# print(number_max)
-
-if number_one >= number_two and number_one >= number_three:
-    print(number_one)
-elif number_two >= number_one and number_two >= number_three:
-    print(number_two)
-else:
-    print(number_three)
+match a:
+    case a if a > 0:
+        print("> 0")
+    case _:
+        print("<= 0")
