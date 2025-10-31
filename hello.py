5a8f64965a1aab7debee5db04b20d3c1ac02d501
Андрей Владимирович Головушкин <ang0l@inbox.ru>
2025-10-31 21:08:14 +0300

    Строки

--- hello.py
+++ hello.py
@@ -1,17 +1,19 @@
-t = (1, 3, 54)
-t1 = 1, 3, 5
+name = "Andrey"
+print(type(name))
+print(name[0])
+# print(name[10])
+# name[0] = "U"
+print(len(name))
+print(name[-1])
 
-print(t[0])
-print(t[-1])
+t = tuple(name)
+l = list(name)
 
-t = (1, "1", [1, 2])
 print(t)
-t[-1][0] = 8
-print(t)
-print(len(t))
-
-l = list(t)
 print(l)
 
-t = tuple(l)
-print(t)
+sep = "=" * 20
+print(sep)
+
+tu = (1, 2) * 20
+print(tu)
