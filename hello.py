a853a3ab990bb9523fd4fabd159c0969c3ff1d08
Головушкин Андрей Владимирович <ang0l@inbox.ru>
2025-11-05 14:08:04 +0300

    Slicing

--- hello.py
+++ hello.py
@@ -1,15 +1,29 @@
-# a = 10
-# b = a
-# a = 7
-# print(a)
-# print(id(a))
-# print(b)
-# print(id(b))
+# a = [1, 2, 3, 4, 5]
+# print(a[0:3])
+# print(a[:3])
+# print(a[2:])
+# print(a[:])
 
-a = [10, 0]
-b = a
-a[0] = 3
+# b = a[:]
+# a[0] = 10
+# print(a)
+# print(b)
+
+a = [[0, 1], 2, 3, 4, 5]
+print(a[0:3])
+print(a[:3])
+print(a[2:])
+print(a[:])
+
+b = a[:]
+a[0][0] = 10
 print(a)
-print(id(a))
 print(b)
-print(id(b))
+
+c = [0, 1, 2, 3, 4, 5, 6]
+print(c[0:5:2])
+print(c[0::2])
+print(c[0::3])
+print(c[-4:-1:2])
+print(c[:4:-1])
+print(c[-1:-4:-1])
