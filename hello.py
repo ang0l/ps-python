402b4b5f8dc870a1b1c17b1bcb953838edb65c10
Головушкин Андрей Владимирович <ang0l@inbox.ru>
2025-11-05 12:59:40 +0300

    Mutable и Immutable

--- hello.py
+++ hello.py
@@ -1,9 +1,15 @@
-a = 10
+# a = 10
+# b = a
+# a = 7
+# print(a)
+# print(id(a))
+# print(b)
+# print(id(b))
+
+a = [10, 0]
+b = a
+a[0] = 3
 print(a)
 print(id(a))
-
-b = a
-a = 7
 print(b)
 print(id(b))
-print(id(a))
