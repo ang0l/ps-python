12f8e3da1acfe3ecb2691324e764901e5b4b6a38
Головушкин Андрей Владимирович <ang0l@inbox.ru>
2025-11-06 15:04:05 +0300

    Строки и Unicode

--- hello.py
+++ hello.py
@@ -1,11 +1,4 @@
-# Нужно просуммировать края и все, что в середине
-# [10, 2, 3, 5] -> (15, 5)
-# [1, 2] -> (3, 0)
-res = sum([1, 2, 3])
-print(res)
-
-# l = [10, 2, 3, 5]
-l = [1, 2]
-first, *rest, last = l
-result = (first + last, sum(rest))
-print(result)
+print(ord("A"))
+print(ord("a"))
+print(ord("П"))
+print(chr(128640))
