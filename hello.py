1b31f3468015afbb3b852c5b8745c2d4bc879dc4
Головушкин Андрей Владимирович <ang0l@inbox.ru>
2025-11-06 15:27:45 +0300

    Изменение строк

--- hello.py
+++ hello.py
@@ -1,4 +1,13 @@
-print(ord("A"))
-print(ord("a"))
-print(ord("П"))
-print(chr(128640))
+password = "hello, PurpleSchool "
+print(password.upper())
+print(password.capitalize())
+print(password.lower())
+print(password.strip())
+print(password.replace("P", "J"))  # замена что меняем, на что меняем
+print(password.split(","))
+
+multiline = """Line 1
+Line 2
+Line 3"""
+print(multiline)
+print(multiline.splitlines())
