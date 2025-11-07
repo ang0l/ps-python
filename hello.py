72a814697493c981204c0abd95a4f5715f722b1c
Головушкин Андрей Владимирович <ang0l@inbox.ru>
2025-11-07 10:48:50 +0300

    Методы строк

--- hello.py
+++ hello.py
@@ -1,13 +1,25 @@
-password = "hello, PurpleSchool "
-print(password.upper())
-print(password.capitalize())
-print(password.lower())
-print(password.strip())
-print(password.replace("P", "J"))  # замена что меняем, на что меняем
-print(password.split(","))
+password = "hello, PurpleSchool! "
+# Возвращает количество вхождений (int)
+print(password.count("o"))
+# Проверяет заканчивается ли строка указкнным символом (bool)
+print(password.endswith("h"))
+# Проверяет начинается ли строка указкнным символом (bool)
+print(password.startswith("h"))
+# Возвращает индеск первого вхождения в строке (int)
+print(password.find("o"))
+# Возвращает индеск первого вхождения в строке начиная от указанного индекса (int)
+print(password.find("o", 10))
+print(password.find("---[]"))  # Вернет "-1" (нет вхождений) (int)
+print(password.index("o"))  # Работает аналогично методу find()
+# Возвращает индекс первого вхождения символа в строке справа. Вернет 17, т.к. первое вхождение символа "o" справа имеет индекс 17
+print(password.rfind("o"))
+# Проверяет является ли значение числом. (bool). Вернет False
+print(password.isnumeric())
+print("123".isnumeric())  # Вернет True
 
-multiline = """Line 1
-Line 2
-Line 3"""
-print(multiline)
-print(multiline.splitlines())
+t = password.split("o")  # Разделит строку по заданному символу. (list)
+print(t)
+
+role = ("Admin", "User")
+all_role = " ".join(role)  # Соединяет значения в tuple, list в одну строку
+print(all_role)
