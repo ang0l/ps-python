21f4e44b483bb695652b1e680f41f11857db12f3
Андрей Владимирович Головушкин <ang0l@inbox.ru>
2025-11-09 21:01:17 +0300

    Escape Characters

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
-name, email, age = "Andrey", "a@a.ru", 56
-print("Привет " + name + " с почтой " + email + " и " + str(age) + " лет!")
-print("Привет {} с почтой {} и {} лет!".format(name, email, age))
-print("Привет {n} с почтой {e} и {a} лет!".format(e=email, a=age, n=name))
-print(f"Привет {name} с почтой {email} и {age - 1} лет!")
-print(f"Привет {name} с почтой {email} и {(age - 1):.2f} лет!")
+text = "Привет ООО \"Рога и копыта\"\n" \
+    "Как дела"
+print(text)
+print("Привет!\rK")  # Перевод коретки
+print("Привет!\tK")  # Табуляция
+print("Привет!\bK")  # backspace. удалит восклицательный знак
