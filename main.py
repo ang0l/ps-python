68bc5e3aea17497b8f749326a4ddecc702c9edfc
Головушкин Андрей Владимирович <ang0l@inbox.ru>
2025-11-12 08:16:34 +0300

    Упражнение - Калькулятор

--- main.py
+++ main.py
@@ -1,14 +1,46 @@
-def print_hello(user_name: str):
-    print(f"Привет {user_name}")
+# Написать функцию calculate, которая принимает 2 числа и операцию
+# "+", "-", "*", "/".
+
+# < преподаватель
+# def calculate(a: int, b: int, operation: str):
+#     operations = ["+", "-", "*", "/"]
+
+#     if operation not in operations:
+#         sum = "Неверная операция"
+#     if operation == "/" and b == 0:
+#         sum = "Деление на ноль"
+
+#     match operation:
+#         case "+":
+#             sum = a + b
+#         case "-":
+#             sum = a - b
+#         case "*":
+#             sum = a * b
+#         case _:
+#             sum = a / b
+
+#     return sum
+
+def calculate(a: float, b: float, operation: str):
+    match operation:
+        case "+":
+            return a + b
+        case "-":
+            return a - b
+        case "*":
+            return a * b
+        case "/":
+            return a / b if b != 0 else "Ошибка: деление на ноль"
+        case _:
+            return "Неизвестная ошибка"
+
+# преподаватель >
 
 
-print_hello("Андрей")
-print_hello("Вася")
-
-
-def multiply(a: int, b: int) -> int:
-    return a * b
-
-
-res = multiply(4, 5)
-print(res)
+print(calculate(2, 5, "+"))
+print(calculate(2, 5, "-"))
+print(calculate(2, 5, "*"))
+print(calculate(2, 5, "/"))
+print(calculate(2, 0, "/"))
+print(calculate(2, 5, "?"))
