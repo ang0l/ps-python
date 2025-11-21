db88983e3b3d0843e024b2f4d90ce275882680af
Андрей Владимирович Головушкин <ang0l@inbox.ru>
2025-11-21 07:16:27 +0300

    Упражнение - Постетители конференции

--- main.py
+++ main.py
@@ -1,26 +1,36 @@
-"""Операции над множествами
-Union difference и другие
+"""Упражнение
+Посетители конференции
 """
 
-a = {1, 2, 3}
-b = {3, 4, 5}
+# Посчитать для каждого дня - всего визитов, уникальных визитов.
+# Найти ID, кто посетили оба дня
+# Найти ID, кто посетили только первый день
+# Найти ID, кто посетили только второй день
+# Найти ID, кто были только 1 раз
 
-# Объединение множеств
-print(a | b)
-print(a.union(b))  # итентичная запись с методом union()
+visitors_day1 = [101, 102, 103, 101, 104, 102, 105, 101]
+visitors_day2 = [101, 108, 100, 101, 105, 107]
 
-# Пересечение множеств
-print(a & b)
-print(a.intersection(b))  # итентичная запись с методом intresection()
+s_visitors_day1 = set(visitors_day1)
+s_visitors_day2 = set(visitors_day2)
 
-a = {1, 2, 3, 4}
-b = {3, 4, 5}
+# < преподаватель
+# подсчет количества посетителей для каждого дня
+print(
+    f"Входов в день 1: {len(visitors_day1)}, уникальных {len(s_visitors_day1)}")
+print(
+    f"Входов в день 2: {len(visitors_day2)}, уникальных {len(s_visitors_day2)}")
+# преподаватель >
 
-# Разница множеств
-print(a - b)
-print(b - a)
+# < преподаватель
+# не делает запись ID уникальных за все дни.
+# print(f"Всего уникальных визитов: {s_visitors_day1.union(s_visitors_day2)}")
+# преподаватель >
 
-# Симметричная разница
-print(a ^ b)
-# итентичная запись с методом symmetric_difference()
-print(a.symmetric_difference(b))
+print(f"Оба дня посещали: {s_visitors_day1.intersection(s_visitors_day2)}")
+print(
+    f"Только первый день посещали: {s_visitors_day1.difference(s_visitors_day2)}")
+print(
+    f"Только второй день посещали: {s_visitors_day2.difference(s_visitors_day1)}")
+print(
+    f"только один раз были: {s_visitors_day1.symmetric_difference(s_visitors_day2)}")
