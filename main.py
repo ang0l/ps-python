5c3d575f6c9d9a467b9665d4695d46dc9e47dbb4
Головушкин Андрей Владимирович <ang0l@inbox.ru>
2025-11-24 13:12:55 +0300

    Упражнение - Нормализация данных

--- main.py
+++ main.py
@@ -1,38 +1,21 @@
 """
-Map
-возводим в квадрат числа в списке
+Упражнение - Нормализация данных
 """
 
-num = [1, 2, 3, 4, 5]
+# Нужно нормвлизовать даннные, убрав пробелы и приведя age к числу
+users = [
+    {"name": "  андрей ", "age": "55"},
+    {"name": "ИРИНА", "age": "56"},
+    {"name": "Станислав", "age": "37"},
+]
 
-# Ранее я сделал бы так:
-# res = []
 
-# for n in num:
-#     res.append(n * n)
+def normalize_data(user) -> dict:
+    return {
+        "name": user["name"].lstrip().capitalize(),
+        "age": int(user["age"].lstrip())
+    }
 
-# print(res)
 
-# Сейчас сделал бы так:
-# def square(x: float):
-#     return x * x
-
-# squares = list(map(square, num))
-
-# вывод в столбик, часть 1
-# squares_iterator = map(lambda x: x * x, num)
-# но еще лучше сделать так:
-squares = list(map(lambda x: x * x, num))
-
-print(squares)
-
-# вывод в столбик, часть 2
-# for item in squares_iterator:
-#     print(item)
-
-# использование функции map() с двумя аргументами
-a = [1, 2, 3]
-b = [10, 20, 30]
-
-sums = list(map(lambda x, y: x + y, a, b))
-print(sums)
+normalized_users = list(map(normalize_data, users))
+print(normalized_users)
