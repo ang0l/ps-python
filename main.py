7845c0f0592cec914abd385f1d05053081599105
Головушкин Андрей Владимирович <ang0l@inbox.ru>
2025-11-24 15:46:56 +0300

    Filter

--- main.py
+++ main.py
@@ -1,21 +1,28 @@
 """
-Упражнение - Нормализация данных
+filter
 """
 
-# Нужно нормвлизовать даннные, убрав пробелы и приведя age к числу
-users = [
-    {"name": "  андрей ", "age": "55"},
-    {"name": "ИРИНА", "age": "56"},
-    {"name": "Станислав", "age": "37"},
-]
+num = [10, 15, 20, 25, 30]
+
+# evens = []
+# for n in num:
+#     if n % 2 == 0:
+#         evens.append(n)
+
+# print(evens)
 
 
-def normalize_data(user) -> dict:
-    return {
-        "name": user["name"].lstrip().capitalize(),
-        "age": int(user["age"].lstrip())
-    }
+# def is_even(x: int) -> bool:
+#     if x % 2 == 0:
+#         return True
+#     return False
 
 
-normalized_users = list(map(normalize_data, users))
-print(normalized_users)
+# evens = list(filter(is_even, num))
+# print(evens)
+
+evens = list(filter(lambda x: x % 2 == 0, num))
+print(evens)
+
+even_comp = [x for x in num if x % 2 == 0]
+print(even_comp)
