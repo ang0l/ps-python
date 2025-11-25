a0cf3ecbababac2d5a41ac8d1b87e0dab629d15f
Головушкин Андрей Владимирович <ang0l@inbox.ru>
2025-11-25 10:57:21 +0300

    Reduce

--- main.py
+++ main.py
@@ -1,31 +1,36 @@
 """
-Упражнение - Поиск заказов
+Reduce
 """
-
-# Нужно найти те заказы, дял которых сумма > 100 и статус paid
-
-orders = [
-    {"id": 1, "user": "Андрей", "amount": 150, "status": "paid"},
-    {"id": 2, "user": "Ирина", "amount": 50, "status": "canceled"},
-    {"id": 3, "user": "Станислав", "amount": 200, "status": "paid"},
-    {"id": 4, "user": "Сергей", "amount": 0, "status": "draft"},
-    {"id": 5, "user": "Екатерина", "amount": 120, "status": "paid"}
-]
-
-# < препадаватель
-# использовал лямбда-функцию
-# def filter_order(order: dict) -> bool:
-#     if order["amount"] > 100 and order["status"] == "paid":
-#         return True
-#     return False
+from functools import reduce
 
 
-# filtered_orders = list(filter(filter_order, orders))
+nums = [1, 2, 3, 4, 5]
+print(sum(nums))
 
-filtered_orders = list(filter(
-    lambda fo: fo["status"] == "paid" and fo["amount"] > 100,
-    orders
-))
-# преподаватель >
+res = 0
+for n in nums:
+    res += n
+print(res)
 
-print(filtered_orders)
+total = reduce(lambda a, b: a + b, nums)
+print(total)
+
+words = ["Привет!", "Как", "дела?"]
+sentence = reduce(lambda a, b: a + " " + b, words)
+print(sentence)
+
+
+# Разбор, что такое a и b в лямбде
+# в функцию передается данные из итератора: аккумулятор и следующий элемент:
+# сначала acc = 0 и к нему прибавляется 1 (из нашего примера)
+# далее к acc = 1 прибавляется следующий 2 и acc = 3
+# далее к acc = 3 прибавляется следующий 4 и acc = 7
+# и т.д
+# acc можно задать начальное значение, например 7 в функции reduce и acc будет изначально равен 7
+# и итоговое значение будет не 15, а 22
+def sum_custom(acc: int, next_item: int):
+    return acc + next_item
+
+
+total = reduce(sum_custom, nums, 7)
+print(total)
