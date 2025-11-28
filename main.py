21b0a915bcceefccc88b0bc0d5907e0d7aa96f53
Андрей Владимирович Головушкин <ang0l@inbox.ru>
2025-11-28 21:12:24 +0300

    Замыкание

--- main.py
+++ main.py
@@ -1,29 +1,34 @@
 """
-Упражнение - Выплата зарплаты
+Замыкание
 """
 
-# Оставь только name и salary
-# Остать только активных
-# Их отсортируй по уменьшению зарплаты и выведи
-# Посчитай сумму для выплат
 
-from functools import reduce
+def multiplier(factor: float):
+    def inner(x: float):
+        return x * factor
+    return inner
 
 
-employees = [
-    {'name': 'Андрей', 'departament': 'IT', 'salary': 120000, 'active': True},
-    {'name': 'Ирина', 'departament': 'HR', 'salary': 90000, 'active': False},
-    {'name': 'Станислав', 'departament': 'IT', 'salary': 150000, 'active': True},
-    {'name': 'Александра', 'departament': 'Finance',
-        'salary': 110000, 'active': True},
-    {'name': 'Екатерина', 'departament': 'IT', 'salary': 95000, 'active': False},
-    {'name': 'Сергей', 'departament': 'Finance', 'salary': 130000, 'active': True},
-]
+double = multiplier(2)
+triple = multiplier(3)
 
-employees_filter = filter(lambda a: a['active'], employees)
-employees_filter = map(
-    lambda a: {'name': a['name'], 'salary': a['salary']}, employees_filter)
-employees_filter = sorted(employees_filter, key=lambda ef: -ef['salary'])
-salary = reduce(lambda a, b: a + b['salary'], employees_filter, 0)
-print(employees_filter)
-print(salary)
+print(double(10))
+print(triple(10))
+
+
+def make_counter():
+    count = 0
+
+    def counter():
+        nonlocal count  # объявил переменную глобальной
+        count += 1
+        return count
+    return counter
+
+
+c1 = make_counter()
+c2 = make_counter()
+print(c1())
+print(c1())
+print(c1())
+print(c2())
