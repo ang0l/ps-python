c91fddd5455a80754f41d9c95b2c3278954c0919
Головушкин Андрей Владимирович <ang0l@inbox.ru>
2025-12-01 15:45:00 +0300

    Упражнение - KeyboardInterrupt

--- main.py
+++ main.py
@@ -6,24 +6,30 @@
 def main():
     print('Task менеджер. help - для справки')
     while True:
-        raw = input("> ").strip()
-        parts = raw.split()
-        cmd, args = parts[0], parts[1:]  # add name="aaaa"
-        match cmd:
-            case 'help':
-                pass
-            case 'add':
-                pass
-            case 'remove':
-                pass
-            case 'edit':
-                pass
-            case 'tags':
-                pass
-            case 'exit':
-                break
-            case _:
-                print('Неизвестная команда')
+        try:
+            raw = input("> ").strip()
+            parts = raw.split()
+            cmd, args = parts[0], parts[1:]  # add name="aaaa"
+            match cmd:
+                case 'help':
+                    pass
+                case 'add':
+                    pass
+                case 'remove':
+                    pass
+                case 'edit':
+                    pass
+                case 'tags':
+                    pass
+                case 'exit':
+                    break
+                case _:
+                    print('Неизвестная команда')
+        except KeyboardInterrupt:
+            print('\nЗавешение приложения')
+            break
+        except Exception as e:
+            print(f'[ERROR]: {e}')
 
 
 main()
